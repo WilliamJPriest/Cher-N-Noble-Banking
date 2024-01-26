413919afc76cf2e16426f91b5d678b8d8160d82e
williamjpriest <liuliu6wp@gmail.com>
2024-01-26 13:40:16 -0500

    image-issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <nav className="flex justify-around bg-lime-800 w-[100vw] h-10">
+    <main className="">
+      <nav className="flex justify-around bg-lime-800 w-screen h-10">
         <h1 className="nuclear-text md:text-3xl pl-2">Cher & Noble Banking <span className="md:text-4xl text-2xl meltdown-text hover:text-rose-900">☢</span></h1>
         <ul className="pt-3 flex gap-2">
           <li><a href="">Banking</a></li>
@@ -13,6 +13,10 @@ export default function Home() {
         </ul>
         <a className="pt-3" href="">Login</a>
       </nav>
+      <section className="bg-nuclear-family bg-cover h-32 w-screen flex items-center justify-center" aria-label="Hero">
+        <img src="" alt="" />
+      </section>
+
     </main>
   );
 }
