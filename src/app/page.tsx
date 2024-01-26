c085741fa515d786cb872d2de9e9756706a9444a
williamjpriest <liuliu6wp@gmail.com>
2024-01-26 13:47:39 -0500

    nuke shadow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="">
-      <nav className="flex justify-around bg-lime-800 w-screen h-10">
+      <nav className="flex justify-around bg-lime-800 w-screen h-10 shadow-lg shadow-[#2CFA1F]">
         <h1 className="nuclear-text md:text-3xl pl-2">Cher & Noble Banking <span className="md:text-4xl text-2xl meltdown-text hover:text-rose-900">☢</span></h1>
         <ul className="pt-3 flex gap-2">
           <li><a href="">Banking</a></li>
