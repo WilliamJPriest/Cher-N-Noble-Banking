bb25ea396fe128a8892ffa560d5630ef6f6eeb21
williamjpriest <liuliu6wp@gmail.com>
2024-01-27 16:47:45 -0500

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="">
-      <nav className="flex justify-around bg-lime-800 w-screen h-10">
+      <nav className="flex justify-around bg-lime-800 w-screen h-[12] shadow-lg shadow-[#3f6212]">
         <h1 className="nuclear-text md:text-3xl pl-2">Cher & Noble Banking <span className="md:text-4xl text-2xl meltdown-text hover:text-rose-900">☢</span></h1>
         <ul className="pt-3 flex gap-2">
           <li><a href="">Banking</a></li>
@@ -12,13 +12,13 @@ export default function Home() {
           <li><a href="">Why Us?</a></li>
         </ul>
       </nav>
-      <section className="shadow shadow-[#2CFA1F] flex justify-between bg-white w-screen" aria-label="Hero-Container">
-          <form className="flex flex-col justify-center align-center mx-auto" action="">
-            <label className="text-black " htmlFor="">Username 
-            <input className="ml-2" type="text" placeholder="username" /></label>
-            <label className="text-black" htmlFor="">Password 
-            <input className="ml-2"  type="password" placeholder="password" /></label>
-            <button className="text-black border-2 border-[#2CFA1F] w-32">Sign In</button>
+      <section className="mt-2 flex justify-between bg-white w-screen h-screen" aria-label="Hero-Container">
+          <form className="flex flex-col justify-center items-center border w-[33vw]" action="">
+            <label className="text-black " htmlFor="">Username: 
+            <input className="w-32 border" type="text" placeholder=" username" /></label>
+            <label className="text-black" htmlFor="">Password: 
+            <input className="w-32 border"  type="password" placeholder=" password" /></label>
+            <button className="text-black border-2 border-[#2CFA1F] w-32 mt-2">Sign In</button>
           </form>
         <img className="" src="https://i.ibb.co/cFW83R6/babuska.jpg" alt="Invest Babuska" />
       </section>
