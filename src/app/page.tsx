9f5d32541ab70e9b16f4f69ce657757e1c1162c7
williamjpriest <liuliu6wp@gmail.com>
2024-01-27 16:07:35 -0500

    babuska

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="">
-      <nav className="flex justify-around bg-lime-800 w-screen h-10 shadow-lg shadow-[#2CFA1F]">
+      <nav className="flex justify-around bg-lime-800 w-screen h-10">
         <h1 className="nuclear-text md:text-3xl pl-2">Cher & Noble Banking <span className="md:text-4xl text-2xl meltdown-text hover:text-rose-900">☢</span></h1>
         <ul className="pt-3 flex gap-2">
           <li><a href="">Banking</a></li>
@@ -13,8 +13,15 @@ export default function Home() {
         </ul>
         <a className="pt-3" href="">Login</a>
       </nav>
-      <section aria-label="Hero">
-        <img className="w-screen h-[500px]" src="https://i.ibb.co/swGgxh9/nuclear-family.webp" alt="The Nuclear Family" />
+      <section className="shadow shadow-[#2CFA1F] flex justify-between bg-white w-screen" aria-label="Hero-Container">
+          <form className="flex flex-col justify-center align-center mx-auto" action="">
+            <label className="text-black " htmlFor="">Username 
+            <input className="ml-2" type="text" placeholder="username" /></label>
+            <label className="text-black" htmlFor="">Password 
+            <input className="ml-2"  type="password" placeholder="password" /></label>
+            <button className="text-black border-2 border-[#2CFA1F] w-32">Sign In</button>
+          </form>
+        <img className="" src="https://i.ibb.co/cFW83R6/babuska.jpg" alt="Invest Babuska" />
       </section>
 
     </main>
