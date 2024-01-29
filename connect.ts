0a32e4a2884b40a3938dc57aa9df7ad8ffd57e02
williamjpriest <liuliu6wp@gmail.com>
2024-01-28 19:43:43 -0500

    connect.js script

---
+++ connect.ts
@@ -0,0 +1,26 @@
+const sqlite3 = require("sqlite3").verbose()
+
+const db = sqlite3.Database("./collection",
+    sqlite3.OPEN_WRITE | sqlite3.OPEN_CREATE,
+    (err:any) =>{
+    if (err){
+        return console.error(err.message);
+    }
+    console.log("logged in")
+}
+);
+
+db.serealize(()=>{
+    db.run(
+        `CREATE TABLE IF NOT EXISTS accounts(
+            id INTEGER PRIMARY KEY,
+            username varchar(20),
+            password varchar(255),
+            amount INTEGER,
+            account_id INTEGER
+
+        )`
+    )
+})
+
+export {}
