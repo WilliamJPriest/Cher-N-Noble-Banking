5a846f1b48e8592934579b373e9184d693003542
williamjpriest <liuliu6wp@gmail.com>
2024-02-02 09:19:22 -0500

    adduser

---
+++ addUser.js
@@ -0,0 +1,18 @@
+const sqlite3 = require("sqlite3").verbose()
+
+const db =new sqlite3.Database("./collection.db",
+    sqlite3.OPEN_READWRITE,
+    (err) =>{
+    if (err){
+        return console.error(err.message);
+    }
+    console.log("logged in")
+}
+);
+function addUser(){
+    db.serialize(()=>{
+        db.run(
+            ``
+        )
+    })    
+}
